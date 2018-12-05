426005362f3dd2cdafbbdab210d65412073ddb5c
Stas Chernenko <chernenkosa@gmail.com>
2018-12-05 15:30:44 +0300

    docs: add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 let gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+   gulp.task('sass', function() {
+       return gulp.src('app/sass/**/*.sass')
+       .pipe(sass())
+       .pipe(gulp.dest('app/css'))
+   }) 
+
+gulp.task('watch', function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+})   
 
